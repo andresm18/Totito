341bd309896e424e5786040e7df945d9fbeda3d7
andresm18 <andresmartinezr@ufm.edu>
2019-04-06 17:45:58 -0600

    principio

--- Totito_AM.py
+++ Totito_AM.py
@@ -1,1 +1,13 @@
-print("hola")
+def tablero():
+    print("aqui iria tablero")
+tablero()
+
+def nombres_juga():
+    jugador_1 = input("Ingrese nombre del primer jugador: ")
+    jugador_2 = input("Ingrese nombre del segundo jugador: ")
+    print(jugador_1,jugador_2)
+nombres_juga()
+
+def totito():
+    tablero()
+    nombres_juga()
