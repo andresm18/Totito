40a44037b464666a2d8ff1de0502e5a982e5a005
andresm18 <andresmartinezr@ufm.edu>
2019-04-06 18:12:08 -0600

    random selecion de quien empieza

--- Totito_AM.py
+++ Totito_AM.py
@@ -1,3 +1,5 @@
+import random
+
 def tablero():
     print("aqui iria tablero")
 tablero()
@@ -5,8 +7,11 @@ tablero()
 def nombres_juga():
     jugador_1 = input("Ingrese nombre del primer jugador: ")
     jugador_2 = input("Ingrese nombre del segundo jugador: ")
+    lista_jugadores = [jugador_1,jugador_2]
     print(jugador_1,jugador_2)
+    print(random.choice(lista_jugadores))
 nombres_juga()
+
 
 def totito():
     tablero()
