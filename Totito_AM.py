3dffba1807b18ef3c5b555e5a3bc9957e1e7ade6
andresm18 <andresmartinezr@ufm.edu>
2019-04-08 21:01:40 -0600

    casi listo :)

--- Totito_AM.py
+++ Totito_AM.py
@@ -47,128 +47,149 @@ def ejemplo_cordenadas():
     tablero[2][2] = " "
     
 
-def espacio_libre(posi,rt):
-    if tablero[posi][rt] == " ":
-        return True
-    else:
-        False
 
 def ganador():
     if tablero[0][0] == "X" and tablero[0][1] == "X" and tablero[0][2] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
     elif tablero[1][0] == "X" and tablero[1][1] == "X" and tablero[1][2] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
     elif tablero[2][0] == "X" and tablero[2][1] == "X" and tablero[2][2] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
     elif tablero[0][0] == "X" and tablero[1][0] == "X" and tablero[2][0] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
     elif tablero[0][1] == "X" and tablero[1][1] == "X" and tablero[2][1] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
     elif tablero[0][2] == "X" and tablero[1][2] == "X" and tablero[2][2] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
     elif tablero[0][0] == "X" and tablero[1][1] == "X" and tablero[2][2] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
     elif tablero[0][2] == "X" and tablero[1][1] == "X" and tablero[2][0] == "X":
         ganador = "si"
-        quien_gan = "Jugador 1"
+        quien_gan = jugador_1
 
     elif tablero[0][0] == "O" and tablero[0][1] == "O" and tablero[0][2] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     elif tablero[1][0] == "O" and tablero[1][1] == "O" and tablero[1][2] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     elif tablero[2][0] == "O" and tablero[2][1] == "O" and tablero[2][2] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     elif tablero[0][0] == "O" and tablero[1][0] == "O" and tablero[2][0] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     elif tablero[0][1] == "O" and tablero[1][1] == "O" and tablero[2][1] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     elif tablero[0][2] == "O" and tablero[1][2] == "O" and tablero[2][2] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     elif tablero[0][0] == "O" and tablero[1][1] == "O" and tablero[2][2] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     elif tablero[0][2] == "O" and tablero[1][1] == "O" and tablero[2][0] == "O":
         ganador = "si"
-        quien_gan = "Jugador 2"
+        quien_gan = jugador_2
     else:
         ganador = "no"
     if ganador == "si":
         print("Termino el juego felicidades a: ",quien_gan)
+        exit(0)
 
 
 def user_choiceP1():
     ganador() 
-    eleccion = input("Ingrese en que posicion quiere poner su pieza:")
-    if eleccion == "q":
-        tablero[0][0] = "X"
-    elif eleccion == "w":
-        tablero[0][1] = "X"
-    elif eleccion == "e":
-        tablero[0][2] = "X"
-    elif eleccion == "a":
-        tablero[1][0] = "x"
-    elif eleccion == "s":
-        tablero[1][1] = "X"
-    elif eleccion == "d":
-        tablero[1][2] = "X"
-    elif eleccion == "z":
-        tablero[2][0] = "X"
-    elif eleccion == "x":
-        tablero[2][1] = "X"
-    elif eleccion == "c":
-        tablero[2][2] = "X" 
-    tabler()
+    valido = "n"
+    while valido != "y":
+        eleccion = input("Ingrese en que posicion quiere poner su pieza: ")
+        if eleccion == "q" and tablero[0][0] == " ":
+            tablero[0][0] = "X"
+            valido = "y"
+        elif eleccion == "w" and tablero[0][1] == " ":
+            tablero[0][1] = "X"
+            valido = "y"
+        elif eleccion == "e" and tablero[0][2] == " ":
+            tablero[0][2] = "X"
+            valido = "y"
+        elif eleccion == "a" and tablero[1][0] == " ":
+            tablero[1][0] = "x"
+            valido = "y"
+        elif eleccion == "s" and tablero[1][1] == " ":
+            tablero[1][1] = "X"
+            valido = "y"
+        elif eleccion == "d" and tablero[1][2] == " ":
+            tablero[1][2] = "X"
+            valido = "y"
+        elif eleccion == "z" and tablero[2][0] == " ":
+            tablero[2][0] = "X"
+            valido = "y"
+        elif eleccion == "x" and tablero[2][1] == " ":
+            tablero[2][1] = "X"
+            valido = "y"
+        elif eleccion == "c" and tablero[2][2] == " ":
+            tablero[2][2] = "X" 
+            valido = "y"
+        else:
+            print("lo siento ya esta tomada la posicion")
+        tabler()
     
 
 def user_choiceP2():
     ganador() 
-    eleccion = input("Ingrese en que posicion quiere poner su pieza:")
-    if eleccion == "q":
-        tablero[0][0] = "O"
-    elif eleccion == "w":
-        tablero[0][1] = "O"
-    elif eleccion == "e":
-        tablero[0][2] = "O"
-    elif eleccion == "a":
-        tablero[1][0] = "O"
-    elif eleccion == "s":
-        tablero[1][1] = "O"
-    elif eleccion == "d":
-        tablero[1][2] = "O"
-    elif eleccion == "z":
-        tablero[2][0] = "O"
-    elif eleccion == "x":
-        tablero[2][1] = "O"
-    elif eleccion == "c":
-        tablero[2][2] = "O"
+    valido = "n"
+    while valido != "y":
+        eleccion = input("Ingrese en que posicion quiere poner su pieza: ")
+        if eleccion == "q" and tablero[0][0] == " ":
+            tablero[0][0] = "O"
+            valido = "y"
+        elif eleccion == "w" and tablero[0][1] == " ":
+            tablero[0][1] = "O"
+            valido = "y"
+        elif eleccion == "e" and tablero[0][2] == " ":
+            tablero[0][2] = "O"
+            valido = "y"
+        elif eleccion == "a" and tablero[1][0] == " ":
+            tablero[1][0] = "O"
+            valido = "y"
+        elif eleccion == "s" and tablero[1][1] == " ":
+            tablero[1][1] = "O"
+            valido = "y"
+        elif eleccion == "d" and tablero[1][2] == " ":
+            tablero[1][2] = "O"
+            valido = "y"
+        elif eleccion == "z" and tablero[2][0] == " ":
+            tablero[2][0] = "O"
+            valido = "y"
+        elif eleccion == "x" and tablero[2][1] == " ":
+            tablero[2][1] = "O"
+            valido = "y"
+        elif eleccion == "c" and tablero[2][2] == " ":
+            tablero[2][2] = "O" 
+            valido = "y"
+        else:
+            print("lo siento ya esta tomada la posicion")
     tabler()
 
-
+jugador_1 = input("Ingrese nombre del primer jugador: ")
+jugador_2 = input("Ingrese nombre del segundo jugador: ")
 
 def totito():
     tabler()
-    jugador_1 = input("Ingrese nombre del primer jugador: ")
-    jugador_2 = input("Ingrese nombre del segundo jugador: ")
     print(jugador_1,jugador_2)
     lista_jugadores = [jugador_1,jugador_2]
     quien_empieza = (random.choice(lista_jugadores))
+    print("Empieza: ",quien_empieza)
     ejemplo_cordenadas()
-    turno = 1
-    while (turno <= 9):
-        if(turno % 2 != 0):
-            print("Es el turno de: " + quien_empieza )
+    while quien_gan != "owo":
+        tabler()
         user_choiceP1()
+        user_choiceP2()
 totito()
